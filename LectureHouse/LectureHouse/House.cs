1d118eba8660af5c41f18ef52706eb0cd9e3c4e1
ProfGriesbauer <josef.griesbauer@hs-kempten.de>
2023-10-17 09:30:38 +0200

    Lecture House 17.10.2023

---
+++ LectureHouse/LectureHouse/House.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureHouse
+{
+    public class House
+    {
+        bool _HauptAllesschalter;
+        bool _HauptLichtschalter;
+
+        float _StromV;
+
+        public bool AllesAus ()
+        {
+            _HauptAllesschalter = false;
+            _HauptLichtschalter = false;
+            return true;
+        }
+
+        public bool GetHauptLichtschalter () //C++-Style
+        {
+            return _HauptLichtschalter;
+        }
+
+        public void SetHauptLichtschalter (bool SchalterZustand) //C++-Style
+        {
+            _HauptLichtschalter = SchalterZustand;
+        }
+
+        public bool HauptLichtschalter //Genau das gleiche wie oben "eigentlich"
+        {
+            get { return _HauptLichtschalter; }
+            set { _HauptLichtschalter = value; }
+        }
+
+        public void Verbrauch (float Menge)
+        {
+            if (Menge < 50)
+            {
+                _StromV = Menge;
+            }
+            else
+            {
+                _StromV = 0;
+            }
+        }
+    }
+}
---
+++ LectureHouse/LectureHouse/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+LectureHouse.House MyHouse1 = new LectureHouse.House();
+LectureHouse.House MyHouse2 = new LectureHouse.House();
+
+MyHouse1.SetHauptLichtschalter(true);
+Console.WriteLine("MyHouse1 HauptLichtschalter: {0}", MyHouse1.GetHauptLichtschalter());
+MyHouse1.AllesAus();
+Console.WriteLine("MyHouse1 HauptLichtschalter: {0}", MyHouse1.HauptLichtschalter);
+
+Console.WriteLine("Hello, World!");
