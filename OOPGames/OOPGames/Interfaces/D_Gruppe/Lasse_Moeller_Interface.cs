c59f2c9012157f0d68f42ae37400131716b748f4
Lasse Moeller <lasse.moeller@stud.hs-kempten.de>
2023-12-04 12:30:45 +0100

    Änderung 04.12

--- OOPGames/OOPGames/Interfaces/D_Gruppe/Lasse_Moeller_Interface.cs
+++ OOPGames/OOPGames/Interfaces/D_Gruppe/Lasse_Moeller_Interface.cs
@@ -11,7 +11,7 @@ namespace OOPGames
     {
         //Indexer: returns 0 for a unused tictactoefield, 1 for player 1, 2 for player 2, etc.
         //indexed by the row r and column c
-        D_MinesweeperField this[int r, int c] { get; set; }
+        D_MinesweeperField this[int r, int s] { get; set; }
     }
 
     public interface ID_MinesweeperPainter : IPaintGame
@@ -28,7 +28,7 @@ namespace OOPGames
         public bool Markiert;
         public int Nachbarminen;
         }
-    public interface D_Minsweeperplayer 
+    public interface D_Minsweeperplayer : IGamePlayer
     {
         void Minsweeperplayer();
     }
