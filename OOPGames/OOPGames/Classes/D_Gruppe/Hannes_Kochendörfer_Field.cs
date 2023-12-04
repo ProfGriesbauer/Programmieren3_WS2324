9cf91d1ef7c269ece55147fb91043344448dc18a
Hannes Kochendörfer <hannes.kochendoerfer@stud.hs-kempten.de>
2023-12-04 13:13:21 +0100

    Field pls work

--- OOPGames/OOPGames/Classes/D_Gruppe/Hannes_Kochendörfer_Field.cs
+++ OOPGames/OOPGames/Classes/D_Gruppe/Hannes_Kochendörfer_Field.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace OOPGames.Classes.D_Gruppe
 {
-    internal class Hannes_Kochendörfer_Field
+    public class D_MinesweeperField
     {
+        public bool Mine;           //1,0
+        public bool Aufgedeckt;     //1,0
+        public bool Markiert;       //1,0
+        public int Nachbarminen;    //0-9
     }
+    public class Hannes_Kochendörfer_MinesweeperField : ID_MinesweeperField
+    {
+        
+        int[,] _Field_Mine = new int[10, 10];
+        int[,] _Field_Aufgedeckt = new int[10, 10];
+        int[,] _Field_Markiert = new int[10, 10];
+        int[,] _Field_Nachbarmine = new int[10, 10];
+        
+        public OOPGames.D_MinesweeperField this[int r, int s]
+        { 
+            get => throw new NotImplementedException();
+          set => throw new NotImplementedException(); 
+        
+        }
+
+        public bool CanBePaintedBy(IPaintGame painter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
