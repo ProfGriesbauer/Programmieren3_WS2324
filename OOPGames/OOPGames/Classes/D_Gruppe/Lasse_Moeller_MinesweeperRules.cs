07b664deef6503d35e9094f7a34d8a49b8245d3b
Thadeus <thadeus.glueck>
2023-12-11 16:35:20 +0100

    Fehler behebung

--- OOPGames/OOPGames/Classes/D_Gruppe/Lasse_Moeller_MinesweeperRules.cs
+++ OOPGames/OOPGames/Classes/D_Gruppe/Lasse_Moeller_MinesweeperRules.cs
@@ -73,12 +73,14 @@ namespace OOPGames
         public void ClearField() 
         
     {
-        for (int r = 1; r < 10; r++)
+        for (int r = 0; r < 10; r++)
         {
             for (int s = 0; s < 10; s++)
             {
                 _Field[r, s].Aufgedeckt = false;
-            }
+                _Field[r, s].Mine = false;
+                _Field[r, s].Markiert = false;
+                }
         }
     }
 
